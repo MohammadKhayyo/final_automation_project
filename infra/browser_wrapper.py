047389a2cd46d8ddb45cff7e1c8e7c7c96884b87
MohammadKhayyo <mohammadkhayyo@gmail.com>
2024-03-11 14:02:14 +0200

    "edit ci.yml"

--- infra/browser_wrapper.py
+++ infra/browser_wrapper.py
@@ -9,23 +9,34 @@ class WebDriverManager:
         config_manager = ConfigurationManager()
         self.settings = config_manager.load_settings()
 
+    def create_options(self, browser_type):
+        if browser_type.lower() == 'chrome':
+            options = webdriver.ChromeOptions()
+        elif browser_type.lower() == 'firefox':
+            options = webdriver.FirefoxOptions()
+        elif browser_type.lower() == 'edge':
+            options = webdriver.EdgeOptions()
+        else:
+            raise ValueError(f"Unsupported browser type: {browser_type}")
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        return options
+
     def initialize_web_driver(self, browser_name):
         if self.settings["grid"]:
             options = self.set_up_capabilities(browser_name)
             self.driver = webdriver.Remote(command_executor=self.settings["hub"], options=options)
         else:
+            options = self.create_options(browser_name)
             if browser_name.lower() == 'chrome':
-                options = webdriver.ChromeOptions()
-                options.add_argument("--headless")
-                options.add_argument("--no-sandbox")  # This line is often necessary in CI environments
-                options.add_argument("--disable-dev-shm-usage")  # This can help in environments with limited resources
                 self.driver = webdriver.Chrome(options=options)
             elif browser_name.lower() == 'firefox':
-                self.driver = webdriver.Firefox()
+                self.driver = webdriver.Firefox(options=options)
             elif browser_name.lower() == 'edge':
-                self.driver = webdriver.Edge()
-        url = self.settings["url"]
-        self.driver.get(url)
+                self.driver = webdriver.Edge(options=options)
+
+        self.driver.get(self.settings["url"])
         self.driver.maximize_window()
         # self.driver.fullscreen_window()
         return self.driver
